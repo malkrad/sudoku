2cfd224cae1e201a3c29990e5379ffc77b17281f
Mo <m.alkrad@hotmail.com>
2020-01-31 12:21:49 +0100

    Updates

---
+++ precache-manifest.362de012c0bc7fd965901f4d8e370f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dd7a2d73ac375185818fe7b8a5e9f0",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "0184108a1b2692e74994",
+    "url": "/sudoku/static/css/main.5ed6e155.chunk.css"
+  },
+  {
+    "revision": "afd29b826439797be7b9",
+    "url": "/sudoku/static/js/2.561b5c15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku/static/js/2.561b5c15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0184108a1b2692e74994",
+    "url": "/sudoku/static/js/main.c9b38099.chunk.js"
+  },
+  {
+    "revision": "ad4a821471ba9147443a",
+    "url": "/sudoku/static/js/runtime-main.125372b0.js"
+  }
+]);
